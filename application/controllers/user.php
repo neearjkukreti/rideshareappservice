1c7391514a76d88aac10a9f4f896c01629ad063d
anubhavbhatt <anubhavfindingmyself@gmail.com>
2015-01-26 21:40:56 +0530

    updaed service user

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,13 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class User extends CI_Controller {
+    const GET_USER_ID               = 'USERID';
+    const GET_FACEBOOK_ID           = 'FACEBOOKID';
+    const STATUS_USER_CREATED       = 'USER_CREATED';
+    const STATUS_USER_VERIFIED      = 'USER_VERIFIED';
+    const STATUS_USER_UPDATED       = 'USER_UPDATED';
+    
+    const ERROR_CREATING_USER       = 'Error in creating user';
     /**
 	 * Search Page for this controller.
 	 *
 	 * Maps to the following URL
 	 * 		http://rideashare.com/index.php/user
-	 *	- or -  
-	 * 		http://rideashare.com/index.php/user/create
 	 *
 	 */
         public function __construct() {
@@ -17,14 +22,39 @@ class User extends CI_Controller {
         
         public function index()
         {
-            
-        }
-        
-        public function create(){
             $post = $this->input->post();
             $userDirty = $post;
             $userdata = $this->cleanUser($userDirty);
-            $this->user_model->create($userdata);
+            
+            $responseData = array();
+            $userid = 0;
+            if( $userid = $this->user_model->isUserExists( $userdata['fb_id'], User_model::FIELD_FACEBOOK_ID ) ){
+                // Old User Updation and Verification here
+                if( isset( $userDirty['needsUpdate']) ){
+                    $this->user_model->update($userid, $userdata);
+                    $responseData['status'] = self::STATUS_USER_UPDATED;
+                }else{
+                    $responseData['status'] = self::STATUS_USER_VERIFIED;
+                }
+            }else{
+                // New User Creation Here
+               $userid = $this->create($userdata); 
+               if($userid){
+                   $responseData['status'] = self::STATUS_USER_CREATED;
+               }else{
+                   $responseData['desc'] = self::ERROR_CREATING_USER;
+               }
+            }
+            if($userid){
+                $userBasicDetail = $this->user_model->read($userid);
+                $responseData['userObject'] = $userBasicDetail;
+            }
+            echo json_encode($responseData);
+        }
+        
+        private function create($userdata){
+            $userid = $this->user_model->create($userdata);
+            return $userid;
         }
         
         private function cleanUser($dirtyData){
@@ -41,14 +71,14 @@ class User extends CI_Controller {
             if(isset($dirtyData['gender'])){
                 $user['gender'] = $dirtyData['gender'];
             }
-            if(!isset($dirtyData['mobile'])){
-                //$user['mobile'] = '9879879870';
+            if(isset($dirtyData['mobile'])){
+                $user['mobile'] = $dirtyData['mobile'];
             }
-            if(!isset($dirtyData['dob'])){
-                //$user['dob'] = '12/01/15';
+            if(isset($dirtyData['dob'])){
+               $user['dob'] = date( "Y-m-d", strtotime($dirtyData['dob']) );
             }
-            if(!isset($dirtyData['email_id'])){
-                //$user['email_id'] = 'test@example.com';
+            if(isset($dirtyData['email_id'])){
+               $user['email_id'] = $dirtyData['dob'];
             }
             return $user;
         }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -6,22 +6,44 @@
  * and open the template in the editor.
  */
 class User_model extends CI_Model {
-    const TABLE_USER = 'app_user';
-
+    const TABLE_USER            = 'user';
+    
+    const FIELD_FACEBOOK_ID     = 'fb_id';
+    const FIELD_ID              = 'id';
+    
     public function __construct(){
 	$this->load->database();
     }
  
     public function create($userdata){
         $this->db->insert( self::TABLE_USER, $userdata );
+        return $this->db->insert_id();
     }
-    public function read(){
-        
+    
+    public function read($id){
+        $sql = sprintf("SELECT * FROM %s WHERE id=%d", self::TABLE_USER, $id);
+        $query = $this->db->query($sql);
+        foreach($query->result() as $row){
+            return $row;
+        }
+        return false;
     }
-    public function update() {
-        
+    
+    public function update($userid, $userdata) {
+        return true;
     }
     public function delete(){
         
+    }
+    
+    public function isUserExists($id, $fieldname){
+        if($fieldname == self::FIELD_FACEBOOK_ID){
+            $sql = sprintf("SELECT * FROM %s WHERE %s=%s", self::TABLE_USER, $fieldname, $id);
+            $result = $this->db->query($sql);
+            if($result->num_rows() == 1){
+                return true;
+            }
+        }
+        return false;       
     }
 }
