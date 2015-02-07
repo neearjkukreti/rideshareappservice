4b619c9eead018145659ea0aee0a844feeaf536d
anubhavbhatt <anubhavfindingmyself@gmail.com>
2015-02-07 12:45:05 +0530

    search controller upated

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -22,78 +22,25 @@ class Search extends CI_Controller {
 	public function index()
 	{
             
-            //$rideDate =  $this->input->post('rideDate');
-	    //$sourceCity = $this->input->post('sourceCity');
-            //$destinationCity = $this->input->post('destinationCity');
-            $rfrom = "Dehradun";
-            $rto = "Delhi";
-            //$rdate = date("Y-m-d",strtotime("24/01/2015"));
-            $rdate = '2015-01-24';
-            $rtime="12:00:00";
-            $searchdata = $this->ride_model->search($rfrom ,$rto ,$rdate ,$rtime );
+        $post = $this->input->post();
+                    
+        if (!isset($HTTP_RAW_POST_DATA)){
+            $HTTP_RAW_POST_DATA = file_get_contents("php://input");
+        }
+        
+        $searchDirty = json_decode($HTTP_RAW_POST_DATA, true); 
+        //$searchDirty = $post;
 
-            $responseData['searchdata']=$searchdata;
+        $rfrom = $searchDirty['rfrom'];
+        $rto   = $searchDirty['rto'];
+        $rdate = $searchDirty['rdate'];
+        $rtime = $searchDirty['rtime'];
+        //$searchOrder = $searchDirty[''];
 
-        /*    $responseData = array(
-                                  "basic" =>  array("SourceCity" => $sourceCity,
-                                                    "DestinationCity" => $destinationCity,
-                                                    "Distance"  => "255.90",
-                                                    "Unit"  => "KM",
-                                                    "TimeinMinutes"  => "285",
-                                                    "AverageFare"   => "650",
-                                                    "Currency"  => "Rupees",
-                                                    "Ridedate"  => "24/01/2015"
-                                                    ),
-                                  "totalAvailabilty" => 23,
-                                  "showResults" => 5,
-                                  "pageNo"  => 1, 
-                                  "availability" => array(
-                                                        "2001" => array(
-                                                                        "Name" => "Dilaram R. Singh",
-                                                                        "ShortDescription" => "I am a IT professional, working as manager",
-                                                                        "Age"   => "34",
-                                                                        "Timein24format"  => "13:30:00",
-                                                                        "ProfilePic" => "http://rideashare.com/images/user/images2001.jpg",
-                                                                        "facebookid" => "100010001001"
-                                                                    ),
-                                                        "2002" => array(
-                                                                        "Name" => "Radhe L. Singh",
-                                                                        "ShortDescription" => "I am a Woodland professional, working as Bootmaker",
-                                                                        "Age"   => "24",
-                                                                        "Timein24format"  => "10:00:00",
-                                                                        "ProfilePic" => "http://rideashare.com/images/user/images2002.jpg",
-                                                                        "facebookid" => "100010001111"
-                                                                    ),
-                                                        "2013" => array(
-                                                                        "Name" => "Rakshat kumar",
-                                                                        "ShortDescription" => "Shop owner",
-                                                                        "Age"   => "38",
-                                                                        "Timein24format"  => "12:00:00",
-                                                                        "ProfilePic" => "http://rideashare.com/images/user/images2013.jpg",
-                                                                        "facebookid" => "100010001341"
-                                                                    ),
-                                                        "2019" => array(
-                                                                        "Name" => "Mohd. RafiQ",
-                                                                        "ShortDescription" => "Flower BoutiQue",
-                                                                        "Age"   => "41",
-                                                                        "Timein24format"  => "09:00:00",
-                                                                        "ProfilePic" => "http://rideashare.com/images/user/images2019.jpg",
-                                                                        "facebookid" => "1009010001341"
-                                                                    ),
-                                                        "2102" => array(
-                                                                        "Name" => "George Hawkins",
-                                                                        "ShortDescription" => "Motor Cyclist",
-                                                                        "Age"   => "31",
-                                                                        "Timein24format"  => "11:00:00",
-                                                                        "ProfilePic" => "http://rideashare.com/images/user/images2102.jpg",
-                                                                        "facebookid" => "100016001341"
-                                                                    ),
-                                                    )
-                            );
-          */
+        $searchdata = $this->ride_model->search($rfrom ,$rto ,$rdate ,$rtime );
 
-
-            echo json_encode($responseData);
+        $responseData['searchdata']=$searchdata;
+        echo json_encode($responseData);
 	}	
 }
 
